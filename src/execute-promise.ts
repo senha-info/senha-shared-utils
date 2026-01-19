1dcf0d71ec1c635e94279da96e443f6704ef7773
Bruno Gaspar <bruninhoogaspar@gmail.com>
2026-01-19 16:32:01 -0300

    chore: add debug option to execute promise

--- src/execute-promise.ts
+++ src/execute-promise.ts
@@ -1,19 +1,26 @@
 import { AxiosError } from "axios";
 import { AppException } from "./exceptions";
 
+interface ExecutePromiseConfig {
+  debug?: boolean;
+}
+
 /**
  * Executes a promise and returns the result, error message and error object.
  *
  * @param {Promise<T>} promise - The promise to execute
+ * @param {ExecutePromiseConfig} [config] - The configuration object
  * @returns {Promise<[T, string | null, any?]>} The result, error message and error object
  */
-async function executePromise<T>(promise: Promise<T>): Promise<[T, string | null, any?]>;
+async function executePromise<T>(promise: Promise<T>, config?: ExecutePromiseConfig): Promise<[T, string | null, any?]>;
 
-async function executePromise<T>(promise: Promise<T>) {
+async function executePromise<T>(promise: Promise<T>, config?: ExecutePromiseConfig) {
   try {
     const result = await promise;
     return [result, null, null];
   } catch (error) {
+    if (config?.debug) console.error(error);
+
     let message = "Error while executing promise";
 
     if (error instanceof Error) {
