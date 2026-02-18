8f508330135a834385e30dd1043ba98cff4e2c62
Bruno Gaspar <bruninhoogaspar@gmail.com>
2026-02-18 12:13:51 -0300

    chore: update metadata function

--- src/generate-metadata-response.ts
+++ src/generate-metadata-response.ts
@@ -1,23 +1,41 @@
-import { parseRequestURL } from "./parse-request-url";
+import { parseRequestURL } from './parse-request-url';
 
 interface GenerateMetadataResponseProps extends Record<string, any> {
+  mode?: 'camel-case' | 'snake-case';
   page: number;
   limit: number;
   count: number;
   baseURL: string;
 }
 
-export function generateMetadataResponse({ page, limit, count, baseURL, ...extra }: GenerateMetadataResponseProps) {
+export function generateMetadataResponse({
+  mode = 'camel-case',
+  page,
+  limit,
+  count,
+  baseURL,
+  ...extra
+}: GenerateMetadataResponseProps) {
   const maxPage = Math.ceil(count / limit);
   const { previousURL, nextURL } = parseRequestURL(page, maxPage, baseURL);
 
   const hasExtra = Object.keys(extra).length > 0;
 
+  if (mode === 'camel-case') {
+    return {
+      count,
+      maxPage,
+      previousURL,
+      nextURL,
+      extra: hasExtra ? extra : undefined,
+    };
+  }
+
   return {
     count,
-    maxPage,
-    previousURL,
-    nextURL,
+    max_page: maxPage,
+    previous_url: previousURL,
+    next_url: nextURL,
     extra: hasExtra ? extra : undefined,
   };
 }
